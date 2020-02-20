b59cb4dc7ccb11f32ed31d50703507816cee13c4
B. van Berkum <dev@dotmpe.com>
2020-02-20 19:23:39 +0100

    CI

--- tools/sh/boot/stderr-console-logger.sh
+++ tools/sh/boot/stderr-console-logger.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sh_include_path= lib_load logger logger-std && {
+lib_load logger logger-std && {
 
   logger_std_init "stderr-console-logger T\$(date +%H:%M:%S)" &&
   export LOG=logger_stderr
--- tools/sh/parts/include.sh
+++ tools/sh/parts/include.sh
@@ -5,6 +5,7 @@ sh_include() # Parts...
   test $# -gt 0 || return
   true ${ci_tools:=tools/ci}
   true ${sh_tools:=tools/sh}
+  test -n "${sh_include_path:-}" || local sh_include_path
   true ${sh_include_path:="{$ci_tools,$sh_tools,$U_S/tools/{ci,sh}}/{parts,boot}"} || return 112
 
   echo "sh_include_path: $sh_include_path '$*'" >&2
@@ -18,12 +19,14 @@ sh_include() # Parts...
       print_err error "" "no sh_include $sh_include_part" "$?" 1
     }
 
+    sh_include_path= \
     . "$base/$sh_include_part.sh" || {
       print_err error "" "at sh_include $sh_include_part" "$?" 1$?
     }
 
     print_err "ok" "" "sh_include_part" "$sh_include_part"
   done
+
 }
 
 #alias sh-parts=sh_include
