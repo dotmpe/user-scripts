e69810f97b4b46b3d09da62e4baee501db35b1d8
B. van Berkum <dev@dotmpe.com>
2020-02-20 21:29:56 +0100

    CI

--- load.bash
+++ load.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+$INIT_LOG "note" "" "Adding SCRIPTPATH" "$(dirname "${BASH_SOURCE[0]}")"
 SCRIPTPATH="$SCRIPTPATH:$(dirname "${BASH_SOURCE[0]}")/src/sh/lib"
 SCRIPTPATH="$SCRIPTPATH:$(dirname "${BASH_SOURCE[0]}")/commands"
