0009d5f2ef6a1fdf0ee8804cc63334221bb60d60
B. van Berkum <dev@dotmpe.com>
2020-02-20 17:34:33 +0100

    CI

--- tools/sh/parts/include.sh
+++ tools/sh/parts/include.sh
@@ -7,17 +7,22 @@ sh_include() # Parts...
   true ${sh_tools:=tools/sh}
   true ${sh_include_path:="{$ci_tools,$sh_tools,$U_S/tools/{ci,sh}}/{parts,boot}"} || return 112
 
+  echo "sh_include_path: $sh_include_path" >&2
   for part in $*
   do
     for base in $(eval echo $sh_include_path)
     do test -e "$base/$part.sh" && break || continue
     done
 
-    test -e "$base/$part.sh" ||
+    test -e "$base/$part.sh" || {
       echo print_err error "" "no sh_include $part" "$?" 1
+      print_err error "" "no sh_include $part" "$?" 1
+    }
 
-    . "$base/$part.sh" ||
+    . "$base/$part.sh" || {
       echo print_err error "" "at sh_include $part" "$?" $?
+      print_err error "" "at sh_include $part" "$?" 1$?
+    }
   done
 }
 
