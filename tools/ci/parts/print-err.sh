9cd3dd468f9dd458bb4e73df3c7ded8abedcdf2d
B. van Berkum <dev@dotmpe.com>
2020-02-20 21:41:19 +0100

    CI

--- tools/ci/parts/print-err.sh
+++ tools/ci/parts/print-err.sh
@@ -7,18 +7,19 @@ print_err()
 
   test -z "${verbosity:-}" -a -z "${DEBUG:-}" && return
   test -n "${2:-}" || set -- "$1" "$base" "$3" "${4:-}" "${5:-}"
-  test -z "${verbosity:-}" -a -n "${DEBUG:-}" || {
+  # XXX:
+  #test -z "${verbosity:-}" -a -n "${DEBUG:-}" || {
 
-    case "$1" in [0-9]* ) true ;; * ) false ;; esac &&
-      lvl=$(log_level_name "$1") ||
-      lvl=$(log_level_num "$1")
+  #  case "$1" in [0-9]* ) true ;; * ) false ;; esac &&
+  #    lvl=$(log_level_name "$1") ||
+  #    lvl=$(log_level_num "$1")
 
-    test $verbosity -ge $lvl || {
-      test -n "${5:-}" && exit $5 || {
-        return 0
-      }
-    }
-  }
+  #  test $verbosity -ge $lvl || {
+  #    test -n "${5:-}" && exit $5 || {
+  #      return 0
+  #    }
+  #  }
+  #}
 
   printf -- "%s\n" "[$2] $1: $3 <${4:-}> (${5:-})" >&2
   test -z "${5:-}" || exit $5 # NOTE: also exit on '0'
