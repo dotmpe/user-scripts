cb6e7cb7f495025e6bb730ed4441a08a7ad3f3b5
B. van Berkum <dev@dotmpe.com>
2020-02-20 17:50:30 +0100

    CI

--- tools/sh/boot/stderr-console-logger.sh
+++ tools/sh/boot/stderr-console-logger.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-lib_load logger logger-std && {
+sh_include_path= lib_load logger logger-std && {
 
   logger_std_init "stderr-console-logger T\$(date +%H:%M:%S)" &&
   export LOG=logger_stderr
