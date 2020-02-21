e04b952675b6022626ab2465a8d531818fd15054
B. van Berkum <dev@dotmpe.com>
2020-02-21 03:57:03 +0100

    CI

--- tools/sh/parts/env-scriptpath-deps.sh
+++ tools/sh/parts/env-scriptpath-deps.sh
@@ -10,13 +10,16 @@ $INIT_LOG note "env-scriptpath-deps" "Current SCRIPTPATH" "$SCRIPTPATH"
 : "${SCRIPTPATH:="$U_S/src/sh/lib"}"
 : "${VND_PATHS:="$(unique-paths ~/build ~/.basher/cellar/packages $VND_GH_SRC $VND_SRC_PREFIX)"}" # /src/*/ )"}"
 
-for supportlib in $(grep '^git ' $CWD/dependencies.txt|cut -d' ' -f2,3);
+for supportlib in $(grep '^git ' $CWD/dependencies.txt|cut -d' ' -f2);
 do
   for vnd_base in $VND_PATHS;
   do
     lib_path="$vnd_base/$supportlib";
+    test -d "$lib_path" && {
+      echo $lib_path
+      ls -la $lib_path/
+    }
     echo . "$lib_path/load.bash";
-    test -d "$lib_path" && ls -la $lib_path/load*
     test -f $lib_path/load.bash || continue
     . "$lib_path/load.bash";
   done;
