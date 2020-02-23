a9d39297fbc39b75bfd77e8b6177b2ff991e8692
B. van Berkum <dev@dotmpe.com>
2020-02-23 22:39:01 +0100

    CI

--- tools/sh/parts/remove-dupes.sh
+++ tools/sh/parts/remove-dupes.sh
@@ -4,7 +4,5 @@ remove-dupes() # ~
   awk '!a[$0]++'
 }
 
-# XXX: move to env init: expand_aliases
-shopt -s expand_aliases
-type remove_dupes || alias remove_dupes=remove-dupes # FIXME: load proper file
+type remove_dupes 2>/dev/null || alias remove_dupes=remove-dupes # FIXME: load proper file
 # Id: U-S:tools/sh/parts/remove-dupes.sh                           vim:ft=bash:
