56ddf3b1e3da04ebc00a5a950ec11e2c59989fdb
B. van Berkum <dev@dotmpe.com>
2020-03-18 01:27:24 +0100

    Run.d

--- src/bash/lib/run-scr.lib.sh
+++ src/bash/lib/run-scr.lib.sh
@@ -1,12 +1,37 @@
 #!/usr/bin/env bash
 
+status=return
+true "${status:=return}"
+
+run-d-lib-init()
+{
+  true "${RUNPATH:="$(run-d-find-path | tr ' ' ':')"}"
+  test -n "$RUNPATH" || return
+}
 
 run()
 {
-  for scriptcmd in "$@"
-  do
-    run-scr "$scriptcmd"
-  done
+  fnmatch "* -- *" " $* " && {
+    local argv=
+    while test $# -gt 0
+    do
+      argv=()
+      while test $# -gt 0 -a "$1" != "--"
+      do
+        argv+=("$1")
+        shift
+      done
+      shift
+      run-scr "${argv[@]}" || return
+    done
+    return
+  } || {
+    local scriptcmd=
+    for scriptcmd in "$@"
+    do
+      run-scr "$scriptcmd" || return
+    done
+  }
 }
 
 run-scr()
@@ -19,10 +44,20 @@ run-scr()
   # TODO: look for first run.d
   test -e "run.d/$scriptcmd.sh" && {
 
-    . ./run.d/$scriptcmd.sh || exit $?
+    . ./run.d/$scriptcmd.sh || $status $?
 
   } || {
 
-    . "$HOME/.conf/dckr/run.d/$scriptcmd.sh" || exit $?
+    . "$HOME/.conf/dckr/run.d/$scriptcmd.sh" || $status $?
   }
+}
+
+run-d-find-path()
+{
+  while test -d $PWD/run.d
+  do
+    printf "$PWD/run.d"
+    cd ..
+    test -d $PWD/run.d && echo ' '
+  done
 }
