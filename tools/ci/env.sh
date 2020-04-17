da6f5500e6bc10f23ee61e391ac441d9e140e5b1
B. van Berkum <dev@dotmpe.com>
2020-04-17 17:39:15 +0200

    Testing

--- tools/ci/env.sh
+++ tools/ci/env.sh
@@ -17,6 +17,7 @@ test "${DEBUG-}" = "1" && set -x
 . "./tools/sh/parts/debug-exit.sh"
 
 . "./tools/sh/parts/env-0-1-lib-sys.sh"
+. "./tools/sh/parts/unique-paths.sh"
 
 ci_env_ts=$($gdate +"%s.%N")
 ci_stages="${ci_stages:-} ci_env"
