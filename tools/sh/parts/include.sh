9c4a3c5a1ba98ba645e0078bce10bb1112fbd31c
B. van Berkum <dev@dotmpe.com>
2020-06-28 00:04:39 +0200

    Rewrote sh-include for use in Sh-mode shell

--- tools/sh/parts/include.sh
+++ tools/sh/parts/include.sh
@@ -6,6 +6,39 @@ sh_include_path()
   sh_include_dry=1 sh_include "$@"
 }
 
+sh_include_path_subdirs()
+{
+  local lang suite langs suites
+  langs="${sh_include_path_langs:-"main ci bash sh"}"
+  suites="${sh_include_path_suites:-"boot parts"}"
+
+  for lang in $langs
+  do
+    for suite in $suites
+    do
+      echo "tools/$lang/$suite"
+    done
+  done
+}
+
+sh_include_path_dirs()
+{
+  local basedir subdir
+
+  test $# -gt 0 || {
+    test "$CWD" = "$(cd $U_S && pwd -P)" &&
+      set -- "$CWD" || set -- "$CWD" "$U_S"
+  }
+
+  for basedir in "$@"
+  do
+    for subdir in $sh_include_path_subdirs
+    do
+      test -e "$basedir/$subdir" || continue
+      echo $basedir/$subdir
+    done
+  done
+}
 
 # Include file by name-id from from $PWD/tools and other tools directories.
 # By default sets sh_include_suites=ci,sh and sh_include_path=$PWD,$U_S.
@@ -13,28 +46,26 @@ sh_include() # Parts...
 {
   test $# -gt 0 || return
 
-  local sh_include_partid sh_include_base
-
-  test -n "${sh_include_subdirs:-}" || {
-    local sh_include_subdirs
-    true "${sh_include_suites:=ci,sh}"
-    sh_include_subdirs="tools/{$sh_include_suites}/{parts,boot}"
+  test -n "${sh_include_path_subdirs:-}" || {
+    local sh_include_path_subdirs
+    sh_include_path_subdirs="$( sh_include_path_subdirs )"
   }
 
   test -n "${sh_include_path:-}" || {
     local sh_include_path
-    test "$(pwd -P)" = "$(cd $U_S && pwd -P)" &&
-      sh_include_path="$(eval echo "$U_S/$sh_include_subdirs")" ||
-      sh_include_path="$(eval echo "{$PWD,$U_S}/$sh_include_subdirs")"
+    sh_include_path_dirs="$( sh_include_path_dirs ${sh_include_path_basedirs:-} )"
   }
+
+  local sh_include_partid sh_include_base
 
   for sh_include_partid in $*
   do
     test -z "${sh_include_debug:-}" ||
       print_err info "" "looking for $sh_include_partid at" "$sh_include_path"
-    for sh_include_base in $sh_include_path
+    for sh_include_base in $sh_include_path_dirs
     do test -e "$sh_include_base/$sh_include_partid.sh" && break || continue
     done
+
     test -e "$sh_include_base/$sh_include_partid.sh" || {
       print_err error "" "no sh_include $sh_include_partid" "$?"
       return 1
