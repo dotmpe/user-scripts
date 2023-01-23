d2839e56699833d19b03186dae426537c021232e
B. van Berkum <dev@dotmpe.com>
2023-01-23 23:53:29 +0100

    t460s

--- test/helper/assert.bash
+++ test/helper/assert.bash
@@ -2,6 +2,6 @@
 
 # XXX: alt test assertion lib: source $VND_SRC_PREFIX/mbland/go-script-bash/lib/bats/assertions
 
-source $VND_SRC_PREFIX/ztombol/bats-support/load.bash
-source $VND_SRC_PREFIX/ztombol/bats-assert/load.bash
-source $VND_SRC_PREFIX/ztombol/bats-file/load.bash
+source ${VND_SRC_PREFIX:?}/ztombol/bats-support/load.bash
+source ${VND_SRC_PREFIX:?}/ztombol/bats-assert/load.bash
+source ${VND_SRC_PREFIX:?}/ztombol/bats-file/load.bash
