2cefae70a739485b416091ce21364038d1d7bac5
B. van Berkum <dev@dotmpe.com>
2023-07-06 22:59:04 +0200

    t460s

--- tools/ci/setup.sh
+++ tools/ci/setup.sh
@@ -7,16 +7,20 @@ export ENV_DEV=1
 export SCRIPT_SHELL=bash
 export CS=dark
 export STATUSDIR_ROOT=$HOME/.local/share/statusdir/
+export U_S=$HOME/project
+export PATH=\$PATH:\$U_S/src/sh/lib:\$U_S/src/bash/lib:\$U_S/commands:\$U_S/contexts
 EOM
-} > ~/.profile
+} >| ~/.profile
 
 { cat <<EOM
 . ~/.profile
-set -euTEo pipefail
-#shopt -s extdebug
-#. ./script/bash-uc.lib.sh
-#trap bash_uc_errexit ERR
-EOM
-} > test-env.sh
 
+. \${U_S:?}/tools/sh/parts/fnmatch.sh
+. \${U_S:?}/tools/sh/parts/sh-mode.sh
+#sh_mode build
+sh_mode strict dev log-init
+EOM
+} >| ./.test-env.sh
+
+echo "Tools CI setup script done" >&2
 # Id: U-S
